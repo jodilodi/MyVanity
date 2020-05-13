abeca8f3380c22f5d0715a6fb4f273a597404f3f
Jodi <jodilodi.lo@gmail.com>
2020-05-13 18:11:51 -0500

    Changed the threshold for the color anaylsis on the python program so now i have vastly more colors causing issues with javascript loading all of the eyeshadows so changed how the colors are viewed and moved things to js file because to much stuff in html now

---
+++ js/menu.js
@@ -0,0 +1,67 @@
+// $("#show-favs").click(function(){
+// 	// var values = {};
+// 	var names = [];
+// 	var keys = Object.keys(localStorage), i = keys.length;
+// 	while(i--){
+// 		if(localStorage.getItem(keys[i]) == 1){
+			
+// 			names.push(keys[i]);
+// 		}
+// 	}
+// 	var allEyeshadows = document.getElementsByClassName("eyeshadow");
+// 	for(var i = 0; i < allEyeshadows.length; i++){
+// 		// console.log(allEyeshadows[i]);
+// 		var thisbrand = allEyeshadows[i].parentElement.id.split('_').join(' ');
+// 		// var thiseyeshadow = allEyeshadows[i].innerText;
+// 		var thiseyeshadow = allEyeshadows[i].classList[1].split('_').join(' ');
+// 		var line = thisbrand + " " + thiseyeshadow;
+// 		if(names.includes(line)){
+// 			allEyeshadows[i].style.display = "block";
+// 		}
+// 		else{
+// 			allEyeshadows[i].style.display = "none";
+// 		}
+// 	}
+// 	//check brand and see if they have any eyeshadows block display
+// 	$.each((window.brands), function(value){
+// 		var branddiv = document.getElementById(window.brands[value]);
+// 		var count = 0;
+// 		$(branddiv).children('.eyeshadow').each(function(){
+// 			if($(this)[0].style.display == "block"){
+// 				count++;
+// 			}
+// 		});
+// 		if(count == 0){
+// 			$(branddiv).hide(500, "swing");
+// 		}
+// 	});
+// });
+// $("#show-all").click(function(){
+	
+// 	var allEyeshadows = document.getElementsByClassName("eyeshadow");
+// 	for(var i = 0; i < allEyeshadows.length; i++){
+// 		allEyeshadows[i].style.display = "block";
+		
+// 	}
+// 	$.each((window.brands), function(value){
+// 		var branddiv = document.getElementById(window.brands[value]);
+// 		$(branddiv).show(500, "swing");
+// 	});
+// });
+
+$("#reset").click(function(){
+	//close pasport as always
+	$("#passport").hide(500, "swing", function(){
+
+		//brands then eyeshadow
+		$.each((window.brands), function(value){	
+			document.getElementById(window.brands[value]).style.display = "block";
+		});
+
+		//eyeshadows
+		var allEyeshadows = document.getElementsByClassName("eyeshadow");
+		for(var i = 0; i < allEyeshadows.length; i++){
+			allEyeshadows[i].style.display = "inline-block";
+		}
+	});
+});
---
+++ js/passport.js
@@ -0,0 +1,119 @@
+var eyeshadowinfo;
+function showPassport(e){
+	eyeshadowDetails = findEyeshadow(this.classList[1].split('_').join(' '));
+		passport = document.getElementById("passport");
+		eyeshadowinfo = this;
+	passport_sparkle_image = document.getElementById("passport_image_sparkle");
+					
+					//get local storage
+	var brand = eyeshadowDetails.brand;
+	var name = eyeshadowinfo.classList[1].split('_').join(' ');
+	var line = brand + " " + name;
+	var result = 0;
+	if(localStorage[line]){
+		result = localStorage[line];
+	}
+
+	var likeicon = document.getElementById("passport-like-icon");
+	if(result == 1){
+	likeicon.classList.remove("far");
+	likeicon.classList.add("fas");
+	}else{
+		likeicon.classList.remove("fas");
+		likeicon.classList.add("far");
+	}
+
+
+	if(passport.style.display == "block"){
+		$(passport).hide(500, "swing", populatePassport);
+	}else{
+		populatePassport();
+	}
+}
+function populatePassport(){
+	passport.style.position = "absolute";
+	passport.style.left = (eyeshadowinfo.getBoundingClientRect().left - 15) + "px";
+	passport.style.top = (eyeshadowinfo.getBoundingClientRect().top - 65 + window.scrollY) + "px";
+
+		// document.getElementById("passport-eyeshadow-name").innerText = eyeshadowinfo.innerText;
+	document.getElementById("passport-eyeshadow-name").innerText = eyeshadowinfo.classList[1].split('_').join(' ');
+	document.getElementById("passport-eyeshadow-color").style.backgroundColor = eyeshadowinfo.style.backgroundColor;
+	document.getElementById("passport-palette-name").innerText = eyeshadowDetails.palette;
+	document.getElementById("passport-brand-name").innerText = eyeshadowDetails.brand;
+	document.getElementById("passport-finish-name").innerText = eyeshadowDetails.finish.charAt(0).toUpperCase() + eyeshadowDetails.finish.slice(1) + " Finish";
+
+	if(eyeshadowDetails.finish == "satin" || eyeshadowDetails.finish == "pearl"){
+		passport_sparkle_image.src = "images/satin.png";
+		passport_sparkle_image.style.visibility = "visible";
+	}else if (eyeshadowDetails.finish != "matte"){
+		passport_sparkle_image.src = "images/sparkle.png";
+		passport_sparkle_image.style.visibility = "visible";
+	}else{
+		passport_sparkle_image.style.visibility = "hidden";
+	}
+	$("#passport").show(500, "swing");
+}
+
+$("#passport-close-icon").click(function(e){
+	$("#passport").hide(500, "swing");
+});
+
+$("#passport-like-icon").click(function(){
+	//get brand and eyeshadow name
+	var brand = document.getElementById("passport-brand-name").innerText;
+	var name = document.getElementById("passport-eyeshadow-name").innerText;
+	var line = brand + " " + name;
+	// localStorage.setItem();
+	//toggle the value here
+	//fas is solid
+	//far is outline
+
+	if (this.classList.contains("fas")){
+		//go to 0
+		localStorage.setItem(line, 0);
+		//switch to far
+		this.classList.remove("fas");
+		this.classList.add("far");
+	}
+	else{
+		//go to 1
+		localStorage.setItem(line, 1);
+		//switch to fas
+		this.classList.remove("far");
+		this.classList.add("fas");
+	}
+});
+
+$("#passport-palette-icon").click(function(){
+	//close the passport
+	$("#passport").hide(500, "swing", function(){
+	//get the palette and brand
+		var palette = document.getElementById("passport-palette-name").innerText;
+		var brand = document.getElementById("passport-brand-name").innerText;
+
+		var allEyeshadows = document.getElementsByClassName("eyeshadow");
+		for(var i = 0; i < allEyeshadows.length; i++){
+			var thiseyeshadow = allEyeshadows[i].classList[1].split('_').join(' ');
+			//find the palette
+			var eyeshadowDetails = findEyeshadow(thiseyeshadow);
+			console.log(eyeshadowDetails.palette);
+
+
+			if(eyeshadowDetails.palette == palette && eyeshadowDetails.brand == brand){
+				allEyeshadows[i].style.display = "inline-block";
+			}else{
+				allEyeshadows[i].style.display = "none";
+			}
+		}
+		//check brand and see if they have any eyeshadows block display
+		//well if brand is not the same as eyeshadow brand then hide
+		$.each((window.brands), function(value){
+			var branddiv = document.getElementById(window.brands[value]);
+			// var count = 0;
+			// console.log(window.brands[value]);
+			if(window.brands[value] != brand){
+				document.getElementById(window.brands[value]).style.display = "none";
+			}
+		});
+	});//end of passport swing	
+});
